5f6cf278321eeee5ffc613679b4abbd85df245da
kwon <kckc0608@naver.com>
2023-03-02 12:23:42 +0900

    FEAT : 헤더 클릭으로 열단위 셀 선택 기능 구현
    헤더를 클릭했을 때 열 단위로 셀을 선택하는 기능 구현

--- shxxt-dev/src/components/ColumnHeaderItem.tsx
+++ shxxt-dev/src/components/ColumnHeaderItem.tsx
@@ -1,32 +1,89 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { PartPropType } from "./Part";
 import { RootState } from "../common/store/store";
+import {
+  startToSetColRange,
+  changeColRange,
+  endToSetColRange,
+} from "../common/store/propsReducer";
 
 const ColumnHeaderContainer = styled.div({
-    display: "flex",
-    width: "5rem",
-    height: "2rem",
-    fontWeight: "bold",
-    alignItems: "center",
-    justifyContent: "center",
+  display: "flex",
+  width: "5rem",
+  height: "2rem",
+  fontWeight: "bold",
+  alignItems: "center",
+  justifyContent: "center",
 });
 
 export default function ColumnHeaderItem({
-    row,
-    col,
-    style,
-    shxxtName,
-}:PartPropType) {
-    const header = useSelector((state: RootState) => {
-        console.log(row, col);
-        return state.PropsListReducer[shxxtName].propsList[row][col].header;
-      });
+  row,
+  col,
+  style,
+  shxxtName,
+}: PartPropType) {
+  const header = useSelector((state: RootState) => {
+    console.log(row, col);
+    return state.PropsListReducer[shxxtName].propsList[row][col].header;
+  });
 
-    return (
-        <ColumnHeaderContainer onClick={(e) => {}}>
-            {header}
-        </ColumnHeaderContainer>
+  const isDragMode = useSelector((state: RootState) => {
+    return state.PropsListReducer[shxxtName].isDragMode;
+  });
+
+  const dispatch = useDispatch();
+
+  const OnMouseEnterColumnHeader = function (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) {
+    if (isDragMode) {
+      dispatch(
+        changeColRange({
+          shxxtName: shxxtName,
+          headerIndex: col,
+        })
+      );
+    }
+  };
+
+  const OnMouseDownColumnHeader = function (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) {
+    e.preventDefault();
+    dispatch(
+      startToSetColRange({
+        shxxtName: shxxtName,
+        headerIndex: col,
+      })
     );
+  };
+
+  const OnMouseUpColumnHeader = function (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) {
+    dispatch(
+      endToSetColRange({
+        shxxtName: shxxtName,
+        headerIndex: col,
+      })
+    );
+  };
+
+  return (
+    <ColumnHeaderContainer
+      onMouseDown={(e) => {
+        OnMouseDownColumnHeader(e);
+      }}
+      onMouseEnter={(e) => {
+        OnMouseEnterColumnHeader(e);
+      }}
+      onMouseUp={(e) => {
+        OnMouseUpColumnHeader(e);
+      }}
+    >
+      {header}
+    </ColumnHeaderContainer>
+  );
 }
